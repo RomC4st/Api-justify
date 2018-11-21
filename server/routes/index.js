fe5f0210d86e440f2673ffd88cbf7b76b57c504a
samirkhennache <samirkhennache@gmail.com>
2018-11-21 18:02:51 +0100

    index modified

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,7 +14,7 @@ const PRIVATE_KEY ='privatekey'
 module.exports = (app) => {
 
 	app.get('/',(req,res) => {
-		res.status(200).send(`<h3>Welcome !</h3>`)
+		res.status(200).send(`<h3> Welcome !!! </h3>`)
 	})
 	/**
 	 * api/register :create a count
