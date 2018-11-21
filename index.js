55a8975bb3dcea3a6ea271cd5d424a00dd985844
samirkhennache <samirkhennache@gmail.com>
2018-11-21 16:14:39 +0100

    code clean

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const models = require('../models')
+const models = require('./models')
 const port = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-require('./routes')(app);
+require('./server/routes')(app);
 
 models.sequelize.sync().then(()=>{
     app.listen(process.env.PORT || port, () => console.log(`LISTENING ON PORT ${port}`));
