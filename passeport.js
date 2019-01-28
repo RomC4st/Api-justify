122b1cd682589c9f387442ee77db90b9295a1c91
samirkhennache <samirkhennache@gmail.com>
2019-01-28 19:18:19 +0100

    aupdate URL

--- passeport.js
+++ passeport.js
@@ -72,7 +72,7 @@ passport.use(
       // callbackURL: configAuth.faceBookAuth.callbackURL
       clientID: "1736117719822046",
       clientSecret: "09ae7dfd4e9578a209db3c1c9f16b474",
-      callbackURL: "http://localhost:3001/api/auth/facebook/callback",
+      callbackURL:"https://justify-text-api.herokuapp.com/api//auth/facebook/callback",
       profileFields: ["emails", "name", "displayName", "profileUrl"]
     },
     function(accessToken, refreshToken, profile, done) {
